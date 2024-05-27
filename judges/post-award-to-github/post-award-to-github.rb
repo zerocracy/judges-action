e7a64c73ea661943e94b3abe7678f2f6540eeaff
Yegor Bugayenko <yegor256@gmail.com>
2024-05-27 15:37:27 +0800

    post

---
+++ judges/post-award-to-github/post-award-to-github.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# MIT License
+#
+# Copyright (c) 2024 Zerocracy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+conclude do
+  on '(and
+    (exists who)
+    (exists when)
+    (exists award)
+    (exists message)
+    (exists issue)
+    (exists repository)
+    (not (exists href)))'
+  consider do |f, _|
+    name = octo.user_name_by_id(f.who)
+    id = octo.add_comment(f.repository, f.issue, "@#{name} #{f.message}")
+    f.href = "https://github.com/#{f.repository}/issues/#{f.issue}/#{id}"
+    $loog.info("Comment ##{id} posted with an award to #{f.href}")
+  end
+end
