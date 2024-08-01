d91a7abf24d49aab05e99c26582a76e2b277b275
Yegor Bugayenko <yegor256@gmail.com>
2024-08-01 22:07:49 +0300

    one more test

---
+++ test/judges/test-label-was-attached.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+# MIT License
+#
+# Copyright (c) 2024 Zerocracy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'factbase'
+require 'loog'
+require 'json'
+require 'minitest/autorun'
+require 'webmock/minitest'
+require 'judges/options'
+
+# Test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024 Yegor Bugayenko
+# License:: MIT
+class TestLabelWasAttached < Minitest::Test
+  def test_catches_label_event
+    WebMock.disable_net_connect!
+    stub_request(:get, 'https://api.github.com/repos/foo/foo').to_return(
+      body: { id: 42, full_name: 'foo/foo' }.to_json, headers: {
+        'content-type': 'application/json'
+      }
+    )
+    stub_request(:get, 'https://api.github.com/repositories/42').to_return(
+      body: { id: 42, full_name: 'foo/foo' }.to_json, headers: {
+        'content-type': 'application/json'
+      }
+    )
+    stub_request(:get, 'https://api.github.com/repositories/42/issues/42/timeline?per_page=100').to_return(
+      body: [{ event: 'labeled', label: { name: 'bug' }, actor: { id: 42 }, created_at: Time.now }].to_json, headers: {
+        'content-type': 'application/json'
+      }
+    )
+
+    # stub_request(:get, 'https://api.github.com/user/42').to_return(
+    #   body: { id: 42, login: 'torvalds' }.to_json, headers: {
+    #     'content-type': 'application/json'
+    #   }
+    # )
+    $fb = Factbase.new
+    $global = {}
+    $local = {}
+    $options = Judges::Options.new({ 'repositories' => 'foo/foo' })
+    $loog = Loog::VERBOSE
+    $judge = 'label-was-attached'
+    op = $fb.insert
+    op.what = 'issue-was-opened'
+    op.repository = 42
+    op.issue = 42
+    require_relative '../../judges/label-was-attached/label-was-attached'
+    f = $fb.query('(eq what "label-was-attached")').each.to_a.first
+    assert(!f.nil?)
+    assert_equal(42, f.who)
+    assert_equal('bug', f.label)
+  end
+end
