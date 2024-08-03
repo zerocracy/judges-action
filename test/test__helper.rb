b8420a7a7bdf13576a23ee16ea34b8dca44f5f8d
Yegor Bugayenko <yegor256@gmail.com>
2024-08-03 16:22:14 +0300

    copyright

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2009-2024 Zerocracy
+# Copyright (c) 2024 Zerocracy
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
