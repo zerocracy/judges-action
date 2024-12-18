197a58482801f91d4cfdf5f3fb326e1fa7c76e81
Yegor Bugayenko <yegor256@gmail.com>
2024-12-18 17:43:29 +0300

    #412: average_release_hoc_size

--- judges/quality-of-service/average_release_hoc_size.rb
+++ judges/quality-of-service/average_release_hoc_size.rb
@@ -22,42 +22,35 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require 'fbe/fb'
 require 'fbe/octo'
-require 'fbe/overwrite'
 require 'fbe/unmask_repos'
-require 'fbe/regularly'
 
-start = Time.now
-Fbe.regularly('quality', 'qos_interval', 'qos_days') do |f|
-  Dir[File.join(__dir__, 'average_*.rb')].each do |rb|
-    n = File.basename(rb).gsub(/\.rb$/, '')
-    next unless f[n].nil?
-    if Fbe.octo.off_quota
-      $loog.info('No GitHub quota left, it is time to stop')
-      break
+# Release hoc and commit size
+#
+# This function is called from the "quality-of-service.rb".
+#
+# @param [Factbase::Fact] fact The fact just under processing
+# @return [Hash] Map with keys as fact attributes and values as integers
+def average_release_hoc_size(fact)
+  repo_releases = {}
+  hocs = []
+  commits = []
+  Fbe.unmask_repos.each do |repo|
+    Fbe.octo.releases(repo).each do |json|
+      break if json[:published_at] < fact.since
+      (repo_releases[repo] ||= []) << json
     end
-    if Time.now - start > 5 * 60
-      $loog.info('We are doing this for too long, time to stop')
-      break
-    end
-    require_relative rb
-    send(n, f).each { |k, v| f = Fbe.overwrite(f, k.to_s, v) }
   end
-
-  # Issue and PR lifetimes:
-  { issue: 'average_issue_lifetime', pr: 'average_pull_lifetime' }.each do |type, prop|
-    ages = []
-    Fbe.unmask_repos.each do |repo|
-      q = "repo:#{repo} type:#{type} closed:>#{f.since.utc.iso8601[0..9]}"
-      ages +=
-        Fbe.octo.search_issues(q)[:items].map do |json|
-          next if json[:closed_at].nil?
-          next if json[:created_at].nil?
-          json[:closed_at] - json[:created_at]
-        end
+  repo_releases.each do |repo, releases|
+    releases.reverse.each_cons(2) do |first, last|
+      Fbe.octo.compare(repo, first[:tag_name], last[:tag_name]).then do |json|
+        hocs << json[:files].map { |file| file[:changes] }.sum
+        commits << json[:total_commits]
+      end
     end
-    ages.compact!
-    f.send("#{prop}=", ages.empty? ? 0 : ages.inject(&:+).to_f / ages.size)
   end
+  {
+    average_release_hoc_size: hocs.empty? ? 0 : hocs.sum.to_f / hocs.size,
+    average_release_commits_size: commits.empty? ? 0 : commits.sum.to_f / commits.size
+  }
 end
