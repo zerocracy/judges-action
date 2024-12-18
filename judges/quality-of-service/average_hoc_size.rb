2cef2aba1854790adfd3ff2af2354a96cd92b0a4
Yegor Bugayenko <yegor256@gmail.com>
2024-12-18 17:36:19 +0300

    #412: average_hoc_size

--- judges/quality-of-service/average_hoc_size.rb
+++ judges/quality-of-service/average_hoc_size.rb
@@ -25,33 +25,27 @@
 require 'fbe/octo'
 require 'fbe/unmask_repos'
 
-# Average review time, review comments, reviewers and reviews
+# Average HOC and number of files changed in recent merged PRs
 #
 # This function is called from the "quality-of-service.rb".
 #
 # @param [Factbase::Fact] fact The fact just under processing
 # @return [Hash] Map with keys as fact attributes and values as integers
-def average_review_time(fact)
-  review_times = []
-  review_comments = []
-  reviewers = []
-  reviews = []
+def average_hoc_size(fact)
+  hocs = []
+  files = []
   Fbe.unmask_repos.each do |repo|
     Fbe.octo.search_issues(
       "repo:#{repo} type:pr is:merged closed:>#{fact.since.utc.iso8601[0..9]}"
-    )[:items].each do |pr|
-      pr_reviews = Fbe.octo.pull_request_reviews(repo, pr[:number])
-      pr_review = pr_reviews.select { |r| r[:submitted_at] }.min_by { |r| r[:submitted_at] }
-      review_times << (pr[:pull_request][:merged_at] - pr_review[:submitted_at]).to_i if pr_review
-      review_comments << Fbe.octo.review_comments(repo, pr[:number]).size
-      reviewers << pr_reviews.map { |r| r.dig(:user, :id) }.uniq.size
-      reviews << pr_reviews.size
+    )[:items].each do |json|
+      Fbe.octo.pull_request(repo, json[:number]).then do |pull|
+        hocs << (pull[:additions] + pull[:deletions])
+        files << pull[:changed_files]
+      end
     end
   end
   {
-    average_review_time: review_times.empty? ? 0 : review_times.sum.to_f / review_times.size,
-    average_review_size: review_comments.empty? ? 0 : review_comments.sum.to_f / review_comments.size,
-    average_reviewers_per_pull: reviewers.empty? ? 0 : reviewers.sum.to_f / reviewers.size,
-    average_reviews_per_pull: reviews.empty? ? 0 : reviews.sum.to_f / reviews.size
+    average_pull_hoc_size: hocs.empty? ? 0 : hocs.sum.to_f / hocs.size,
+    average_pull_files_size: files.empty? ? 0 : files.sum.to_f / files.size
   }
 end
