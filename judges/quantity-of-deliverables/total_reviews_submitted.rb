ffec01de411a2cc6e9a95336c6cf3b7d61e69f50
Yegor Bugayenko <yegor256@gmail.com>
2024-12-19 06:31:37 +0300

    #412: total_reviews_submitted

--- judges/quantity-of-deliverables/total_reviews_submitted.rb
+++ judges/quantity-of-deliverables/total_reviews_submitted.rb
@@ -25,16 +25,20 @@
 require 'fbe/octo'
 require 'fbe/unmask_repos'
 
-# Total number of CI builds executed in all repositories from since
+# Total number of code reviews in all repositories from since
 #
 # This function is called from the "quantity-of-deliverables.rb".
 #
 # @param [Factbase::Fact] fact The fact just under processing
 # @return [Hash] Map with keys as fact attributes and values as integers
-def total_builds_ran(fact)
+def total_reviews_submitted(fact)
   total =
     Fbe.unmask_repos.sum do |repo|
-      Fbe.octo.repository_workflow_runs(repo, created: ">#{fact.since.utc.iso8601[0..9]}")[:total_count]
+      Fbe.octo.pull_requests(repo, state: 'all').sum do |pr|
+        Fbe.octo.pull_request_reviews(repo, pr[:number]).count do |review|
+          review[:submitted_at] && review[:submitted_at] > fact.since
+        end
+      end
     end
-  { total_builds_ran: total }
+  { total_reviews_submitted: total }
 end
