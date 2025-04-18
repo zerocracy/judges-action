1025b97aa2ae9657f885890bfe09bb07f47d91cc
Yegor Bugayenko <yegor256@gmail.com>
2025-04-18 14:24:34 +0300

    #573 delete who

--- judges/who-has-name/who-has-name.rb
+++ judges/who-has-name/who-has-name.rb
@@ -3,17 +3,21 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025 Zerocracy
 # SPDX-License-Identifier: MIT
 
-require 'fbe/octo'
+require 'fbe/delete'
 require 'fbe/fb'
+require 'fbe/octo'
 require 'fbe/overwrite'
 
+# Gets nick name of GitHub user, or NIL if user not found.
+# @param [String] who The ID of GitHub user
+# @return [String] The nick name
 def name_of(who)
   n = Fbe.octo.user_name_by_id(who)
   $loog.debug("User ##{who} is actually @#{n}")
   n
 rescue Octokit::NotFound => e
   $loog.warn("The user ##{who} is absent in GitHub: #{e.message}")
-  'unknown'
+  nil
 end
 
 Fbe.fb.query(
@@ -26,12 +30,17 @@ Fbe.fb.query(
       (eq what '#{$judge}')
       (eq where $where))))"
 ).each do |f|
-  n = Fbe.fb.insert
-  n.what = $judge
-  n.who = f.who
-  n.where = f.where
-  n.when = Time.now
-  n.name = name_of(f.who)
+  nick = name_of(f.who)
+  if nick.nil?
+    Fbe.delete(f, 'who')
+  else
+    n = Fbe.fb.insert
+    n.what = $judge
+    n.who = f.who
+    n.where = f.where
+    n.when = Time.now
+    n.name = nick
+  end
 end
 
 Fbe.fb.query(
@@ -41,5 +50,10 @@ Fbe.fb.query(
     (exists who)
     (lt when (minus (to_time (env 'TODAY' '#{Time.now.utc.iso8601}')) '5 days')))"
 ).each do |f|
-  Fbe.overwrite(f, 'name', name_of(f.who))
+  nick = name_of(f.who)
+  if nick.nil?
+    Fbe.delete(f, 'who')
+  else
+    Fbe.overwrite(f, 'name', nick)
+  end
 end
--- test/judges/test-who-has-name.rb
+++ test/judges/test-who-has-name.rb
@@ -19,10 +19,26 @@ class TestWhoHasName < Jp::Test
     )
     fb = Factbase.new
     f = fb.insert
+    f._id = 333
     f.who = 444
     f.where = 'github'
     load_it('who-has-name', fb)
     assert_equal(2, fb.query('(always)').each.to_a.size)
     assert_equal('lebowski', fb.query('(exists name)').each.to_a.first.name)
+  end
+
+  def test_deletes_who_when_not_found
+    WebMock.disable_net_connect!
+    stub_github(
+      'https://api.github.com/user/444',
+      body: '', status: 404
+    )
+    fb = Factbase.new
+    f = fb.insert
+    f._id = 999
+    f.who = 444
+    f.where = 'github'
+    load_it('who-has-name', fb)
+    assert_empty(fb.query('(exists who)').each.to_a)
   end
 end
