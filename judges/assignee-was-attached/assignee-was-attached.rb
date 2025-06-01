86a4d75b57cc3f182827ec62681d7a6edf317627
Artem Yegorov <yegorov0725@yandex.ru>
2025-06-02 00:20:28 +0300

    #659: fix assignee-was-attached judge

--- judges/assignee-was-attached/assignee-was-attached.rb
+++ judges/assignee-was-attached/assignee-was-attached.rb
@@ -18,24 +18,24 @@ Fbe.conclude do
          (eq where 'github')
          (eq repository #{f.repository})
          (eq issue #{f.issue})
-         (eq what 'issue-was-closed'))"
+         (eq what 'issue-was-assigned'))"
     ).each.to_a.first
     Fbe.octo.issue_events(
       Fbe.octo.repo_name_by_id(f.repository),
       f.issue
-    ).select { _1[:event] == 'assigned' }.each do |event|
-      fact =
-        Fbe.if_absent do |fact|
+    ).find { _1[:event] == 'assigned' }.then do |event|
+      next if event.nil?
+      Fbe.fb.txn do |fbt|
+        fbt.insert.then do |fact|
           fact.where = f.where
           fact.what = 'issue-was-assigned'
           fact.repository = f.repository
           fact.issue = f.issue
-          fact.issue_event_id = event[:id]
+          fact.who = event.dig(:assignee, :id)
+          fact.assigner = event.dig(:assigner, :id)
+          fact.when = event[:created_at]
         end
-      next if fact.nil?
-      fact.who = event.dig(:assignee, :id)
-      fact.assigner = event.dig(:assigner, :id)
-      fact.when = event[:created_at]
+      end
     end
   end
 end
--- test/judges/test-assignee-was-attached.rb
+++ test/judges/test-assignee-was-attached.rb
@@ -21,34 +21,31 @@ class TestAssigneeWasAttached < Jp::Test
       'https://api.github.com/repos/foo/foo/issues/45/events?per_page=100',
       body: [
         {
-          id: 126, actor: { login: 'user', id: 411, type: 'User' },
-          event: 'labeled', created_at: Time.parse('2025-05-30 14:41:00 UTC'),
+          id: 726, actor: { login: 'user', id: 411, type: 'User' },
+          event: 'labeled', created_at: Time.parse('2025-05-30 21:41:00 UTC'),
           label: { name: 'bug', color: 'd73a4a' }
         },
         {
-          id: 339, actor: { login: 'user2', id: 422, type: 'User' },
-          event: 'subscribed', created_at: Time.parse('2025-05-30 14:41:10 UTC')
-        },
-        {
-          id: 490, actor: { login: 'github-actions[bot]', id: 41_898_282, type: 'Bot' },
-          event: 'renamed', created_at: Time.parse('2025-05-30 14:41:30 UTC'),
-          rename: { from: 'some title', to: 'some title 2' }
-        },
-        {
-          id: 505, actor: { login: 'user', id: 411, type: 'User' },
-          event: 'subscribed', created_at: Time.parse('2025-05-30 16:18:24 UTC')
-        },
-        {
           id: 608, actor: { login: 'user2', id: 422, type: 'User' },
-          event: 'assigned', created_at: Time.parse('2025-05-30 17:59:08 UTC'),
+          event: 'assigned', created_at: Time.parse('2025-05-30 20:59:08 UTC'),
           assignee: { login: 'user2', id: 422, type: 'User' },
           assigner: { login: 'user', id: 411, type: 'User' }
         },
         {
-          id: 776, actor: { login: 'user2', id: 422, type: 'User' },
+          id: 539, actor: { login: 'user2', id: 422, type: 'User' },
+          event: 'subscribed', created_at: Time.parse('2025-05-30 15:41:10 UTC')
+        },
+        {
+          id: 408, actor: { login: 'user3', id: 423, type: 'User' },
+          event: 'assigned', created_at: Time.parse('2025-05-29 17:59:08 UTC'),
+          assignee: { login: 'user3', id: 423, type: 'User' },
+          assigner: { login: 'user4', id: 414, type: 'User' }
+        },
+        {
+          id: 376, actor: { login: 'user2', id: 422, type: 'User' },
           event: 'referenced', commit_id: '4621af032170f43d',
           commit_url: 'https://api.github.com/repos/foo/foo/commits/4621af032170f43d',
-          created_at: Time.parse('2025-05-30 19:57:50 UTC')
+          created_at: Time.parse('2025-05-28 19:57:50 UTC')
         }
       ]
     )
@@ -56,46 +53,56 @@ class TestAssigneeWasAttached < Jp::Test
       'https://api.github.com/repos/foo/foo/issues/47/events?per_page=100',
       body: [
         {
-          id: 1110, actor: { login: 'user80', id: 80, type: 'User' },
+          id: 1376, actor: { login: 'user80', id: 80, type: 'User' },
           event: 'assigned', created_at: Time.parse('2025-05-30T15:00:00Z'),
           assignee: { login: 'user80', id: 80, type: 'User' },
           assigner: { login: 'user70', id: 70, type: 'User' }
         },
         {
           id: 1208, actor: { login: 'user2', id: 422, type: 'User' },
-          event: 'assigned', created_at: Time.parse('2025-05-31 17:59:08 UTC'),
+          event: 'assigned', created_at: Time.parse('2025-05-29 17:59:08 UTC'),
           assignee: { login: 'user2', id: 422, type: 'User' },
           assigner: { login: 'user', id: 411, type: 'User' }
         },
         {
-          id: 1376, actor: { login: 'user2', id: 422, type: 'User' },
+          id: 1110, actor: { login: 'user2', id: 422, type: 'User' },
           event: 'referenced', commit_id: '4621af032170f43d',
           commit_url: 'https://api.github.com/repos/foo/foo/commits/4621af032170f43d',
-          created_at: Time.parse('2025-05-31 19:57:50 UTC')
+          created_at: Time.parse('2025-05-28 19:57:50 UTC')
         }
       ]
     )
+    stub_github(
+      'https://api.github.com/repos/foo/foo/issues/49/events?per_page=100',
+      body: [
+        {
+          id: 2111, actor: { login: 'user2', id: 422, type: 'User' },
+          event: 'referenced', commit_id: '4621af032170f43d',
+          commit_url: 'https://api.github.com/repos/foo/foo/commits/4621af032170f43d',
+          created_at: Time.parse('2025-05-28 19:57:50 UTC')
+        }
+      ]
+    )
+    stub_github('https://api.github.com/repos/foo/foo/issues/51/events?per_page=100', body: [])
     fb = Factbase.new
     fb.with(_id: 1, what: 'issue-was-opened', repository: 42, issue: 44, where: 'github')
-      .with(_id: 2, what: 'issue-was-closed', repository: 42, issue: 44, where: 'github')
+      .with(_id: 2, what: 'issue-was-assigned', repository: 42, issue: 44, where: 'github')
       .with(_id: 3, what: 'issue-was-opened', repository: 42, issue: 45, where: 'github')
-      .with(_id: 4, what: 'issue-was-closed', repository: 42, issue: 45, where: 'gitlab')
-      .with(_id: 5, what: 'issue-was-opened', repository: 42, issue: 47, where: 'github')
-      .with(_id: 6, what: 'issue-was-assigned', repository: 42, issue: 47, where: 'github',
-            issue_event_id: 1110, when: Time.parse('2025-05-30T15:00:00Z'), who: 80, assigner: 70)
+      .with(_id: 4, what: 'issue-was-closed', repository: 42, issue: 45, where: 'github')
+      .with(_id: 5, what: 'issue-was-opened', repository: 42, issue: 45, where: 'gitlab')
+      .with(_id: 6, what: 'issue-was-opened', repository: 42, issue: 47, where: 'github')
+      .with(_id: 7, what: 'issue-was-opened', repository: 42, issue: 49, where: 'github')
+      .with(_id: 8, what: 'issue-was-opened', repository: 42, issue: 51, where: 'github')
     load_it('assignee-was-attached', fb)
-    assert_equal(8, fb.all.size)
-    assert_equal(3, fb.picks(what: 'issue-was-opened').size)
-    assert_equal(2, fb.picks(what: 'issue-was-closed').size)
+    assert_equal(10, fb.all.size)
+    assert_equal(6, fb.picks(what: 'issue-was-opened').size)
+    assert_equal(1, fb.picks(what: 'issue-was-closed').size)
     assert_equal(3, fb.picks(what: 'issue-was-assigned').size)
     assert(fb.one?(what: 'issue-was-assigned', repository: 42, issue: 45, where: 'github',
-                   issue_event_id: 608, when: Time.parse('2025-05-30 17:59:08 UTC'),
-                   who: 422, assigner: 411))
+                   when: Time.parse('2025-05-30 20:59:08 UTC'), who: 422, assigner: 411))
     assert(fb.one?(what: 'issue-was-assigned', repository: 42, issue: 47, where: 'github',
-                   issue_event_id: 1110, when: Time.parse('2025-05-30T15:00:00Z'),
-                   who: 80, assigner: 70))
-    assert(fb.one?(what: 'issue-was-assigned', repository: 42, issue: 47, where: 'github',
-                   issue_event_id: 1208, when: Time.parse('2025-05-31 17:59:08 UTC'),
-                   who: 422, assigner: 411))
+                   when: Time.parse('2025-05-30T15:00:00Z'), who: 80, assigner: 70))
+    assert(fb.none?(what: 'issue-was-assigned', repository: 42, issue: 49, where: 'github'))
+    assert(fb.none?(what: 'issue-was-assigned', repository: 42, issue: 51, where: 'github'))
   end
 end
