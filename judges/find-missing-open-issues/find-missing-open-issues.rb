1dcf8f64d869cb4d2d45c1f3e6463035cfd18ca8
Artem Yegorov <yegorov0725@yandex.ru>
2025-06-04 23:57:03 +0300

    #660: fix query in find-missing-open-issues

--- judges/find-missing-open-issues/find-missing-open-issues.rb
+++ judges/find-missing-open-issues/find-missing-open-issues.rb
@@ -11,41 +11,51 @@ require 'fbe/conclude'
 require 'fbe/issue'
 require 'fbe/who'
 
-%w[issue pull].each do |type|
-  what = "#{type}-was-opened"
-  Fbe.conclude do
-    quota_aware
-    on "(and
-          (eq where 'github')
-          (exists repository)
-          (exists issue)
-          (not (eq what '#{what}'))
-          (matches what '#{type}')
-          (empty
-            (and
-              (eq where 'github')
-              (eq repository $repository)
-              (eq issue $issue)
-              (eq what '#{what}'))))"
+Fbe.conclude do
+  quota_aware
+  on "(and
+        (eq where 'github')
+        (exists repository)
+        (exists what)
+        (exists issue)
+        (unique issue)
+        (empty
+          (and
+            (eq where 'github')
+            (eq repository $repository)
+            (eq issue $issue)
+            (eq what 'issue-was-opened')))
+        (empty
+          (and
+            (eq where 'github')
+            (eq repository $repository)
+            (eq issue $issue)
+            (eq what 'pull-was-opened'))))"
 
-    consider do |f|
-      repo = Fbe.octo.repo_name_by_id(f.repository)
-      begin
-        json = Fbe.octo.issue(repo, f.issue)
-      rescue Octokit::NotFound
-        $loog.info("The #{type} ##{f.issue} doesn't exist in #{repo}")
-        next
+  consider do |f|
+    type =
+      if f.what.include?('issue')
+        'issue'
+      elsif f.what.include?('pull')
+        'pull'
       end
-      Fbe.fb.txn do |fbt|
-        fbt.insert.then do |fact|
-          fact.where = 'github'
-          fact.what = what
-          fact.repository = f.repository
-          fact.issue = f.issue
-          fact.when = json[:created_at]
-          fact.who = json.dig(:user, :id)
-          fact.details = "The #{type} #{Fbe.issue(fact)} has been opened by #{Fbe.who(fact)}."
-        end
+    next if type.nil?
+    repo = Fbe.octo.repo_name_by_id(f.repository)
+    begin
+      json = Fbe.octo.issue(repo, f.issue)
+    rescue Octokit::NotFound
+      $loog.info("The #{type} ##{f.issue} doesn't exist in #{repo}")
+      next
+    end
+    Fbe.fb.txn do |fbt|
+      fbt.insert.then do |fact|
+        fact.where = 'github'
+        fact.what = "#{type}-was-opened"
+        fact.repository = f.repository
+        fact.issue = f.issue
+        fact.when = json[:created_at]
+        fact.who = json.dig(:user, :id)
+        fact.details = "The #{type} #{Fbe.issue(fact)} has been opened by #{Fbe.who(fact)}."
       end
     end
   end
