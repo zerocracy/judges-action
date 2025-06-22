0b0003df2f29e2b5469a6db8079ac321190cac14
Yegor Bugayenko <yegor256@gmail.com>
2025-06-22 07:17:57 +0300

    Refactor user status check logic and logging.

--- judges/who-is-alive/who-is-alive.rb
+++ judges/who-is-alive/who-is-alive.rb
@@ -11,31 +11,36 @@
 # @see ../../lib/nick_of.rb Implementation of the nick retrieval logic
 # @see https://github.com/yegor256/fbe/blob/master/lib/fbe/delete.rb Implementation of Fbe.delete
 
+require 'fbe/conclude'
 require 'fbe/fb'
 require 'fbe/octo'
 require_relative '../../lib/nick_of'
 
-users = Fbe.fb.query(
-  "(and
+seen = []
+
+Fbe.conclude do
+  quota_aware
+  on "(and
     (eq where 'github')
     (eq what 'who-has-name')
     (not (exists stale))
     (exists who)
     (lt when (minus (to_time (env 'TODAY' '#{Time.now.utc.iso8601}')) '2 days')))"
-).each.map(&:who).uniq
-
-users.each do |who|
-  nick = Jp.nick_of(who)
-  unless nick.nil?
-    $loog.debug("GitHub user @#{nick} (##{who}) is alive")
-    next
-  end
-  Fbe.fb.query("(and (eq what 'who-has-name') (eq who #{who}))").delete!
-  done =
-    Fbe.fb.query("(eq who #{who})").each do |n|
-      n.stale = "user ##{who}"
+  consider do |f|
+    next if seen.include?(f.who)
+    seen << f.who
+    nick = Jp.nick_of(f.who)
+    unless nick.nil?
+      $loog.debug("GitHub user @#{nick} (##{f.who}) is alive")
+      next
     end
-  $loog.info("GitHub user ##{who} is gone, marked #{done} facts as stale")
+    Fbe.fb.query("(and (eq what 'who-has-name') (eq who #{f.who}))").delete!
+    done =
+      Fbe.fb.query("(eq who #{f.who})").each do |n|
+        n.stale = "user ##{f.who}"
+      end
+    $loog.info("GitHub user ##{f.who} is gone, marked #{done} facts as stale")
+  end
 end
 
 Fbe.octo.print_trace!
