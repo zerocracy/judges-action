6df1707fcc703c5c5a3a94e0bd5da692efa970ef
Yegor Bugayenko <yegor256@gmail.com>
2025-06-22 07:15:35 +0300

    Refactor judge logic for better readability.

--- judges/who-has-name/who-has-name.rb
+++ judges/who-has-name/who-has-name.rb
@@ -11,13 +11,15 @@
 # @see ../../lib/nick_of.rb Implementation of the nick retrieval logic
 # @note This judge runs periodically to ensure all users have up-to-date nicknames recorded
 
+require 'fbe/conclude'
 require 'fbe/fb'
 require 'fbe/octo'
 require 'fbe/overwrite'
 require_relative '../../lib/nick_of'
 
-Fbe.fb.query(
-  "(and
+Fbe.conclude do
+  quota_aware
+  on "(and
     (eq where 'github')
     (exists who)
     (not (exists stale))
@@ -26,28 +28,31 @@ Fbe.fb.query(
       (eq who $who)
       (eq what '#{$judge}')
       (eq where $where))))"
-).each do |f|
-  nick = Jp.nick_of(f.who)
-  next if nick.nil?
-  n = Fbe.fb.insert
-  n.what = $judge
-  n.who = f.who
-  n.where = f.where
-  n.when = Time.now
-  n.name = nick
+  consider do |f|
+    nick = Jp.nick_of(f.who)
+    next if nick.nil?
+    n = Fbe.fb.insert
+    n.what = $judge
+    n.who = f.who
+    n.where = f.where
+    n.when = Time.now
+    n.name = nick
+  end
 end
 
-Fbe.fb.query(
-  "(and
+Fbe.conclude do
+  quota_aware
+  on "(and
     (eq what 'who-has-name')
     (not (exists stale))
     (eq where 'github')
     (exists who)
     (lt when (minus (to_time (env 'TODAY' '#{Time.now.utc.iso8601}')) '5 days')))"
-).each do |f|
-  nick = Jp.nick_of(f.who)
-  next if nick.nil?
-  Fbe.overwrite(f, 'name', nick)
+  consider do |f|
+    nick = Jp.nick_of(f.who)
+    next if nick.nil?
+    Fbe.overwrite(f, 'name', nick)
+  end
 end
 
 Fbe.octo.print_trace!
