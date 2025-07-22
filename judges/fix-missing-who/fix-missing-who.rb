02bc3abc5b6430ab399dbc0e3b95b4a2d8a62b1e
Yegor Bugayenko <yegor256@gmail.com>
2025-07-22 12:28:07 +0300

    #817: Add judge to fix missing 'who' property.

---
+++ judges/fix-missing-who/fix-missing-who.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Zerocracy
+# SPDX-License-Identifier: MIT
+
+# Judge that monitors facts with exists repository and issue properties and
+# create missing [issue/pull]-was-opened fact
+
+require 'octokit'
+require 'fbe/conclude'
+require 'fbe/issue'
+require 'fbe/octo'
+require 'fbe/who'
+
+Fbe.conclude do
+  quota_aware
+  on "(and
+    (eq where 'github')
+    (exists issue)
+    (exists repository)
+    (or (eq what 'issue-was-opened') (eq what 'pull-was-opened'))
+    (not (exists stale))
+    (not (exists who)))"
+  consider do |f|
+    repo = Fbe.octo.repo_name_by_id(f.repository)
+    begin
+      json = Fbe.octo.issue(repo, f.issue)
+    rescue Octokit::NotFound
+      $loog.info("#{Fbe.issue(f)} doesn't exist in #{repo}")
+      f.stale = "issue ##{f.issue}"
+      $loog.info("#{Fbe.issue(f)} is lost")
+      next
+    end
+    a = json.dig(:user, :id)
+    if a.nil?
+      f.stale = 'who'
+      $loog.info("Authorship is lost in #{Fbe.issue(f)}")
+    else
+      f.who = a
+      $loog.info("Authorship is restored in #{Fbe.issue(f)}: #{f.who}")
+    end
+  end
+end
+
+Fbe.octo.print_trace!
