5a42f830765337d5f29ba8e51c252a247bdf44c4
Yegor Bugayenko <yegor256@gmail.com>
2025-07-22 12:31:41 +0300

    #817: Refactor authorship restoration logic.

--- judges/fix-missing-who/fix-missing-who.rb
+++ judges/fix-missing-who/fix-missing-who.rb
@@ -12,32 +12,40 @@ require 'fbe/issue'
 require 'fbe/octo'
 require 'fbe/who'
 
-Fbe.conclude do
-  quota_aware
-  on "(and
-    (eq where 'github')
-    (exists issue)
-    (exists repository)
-    (or (eq what 'issue-was-opened') (eq what 'pull-was-opened'))
-    (not (exists stale))
-    (not (exists who)))"
-  consider do |f|
-    repo = Fbe.octo.repo_name_by_id(f.repository)
-    begin
-      json = Fbe.octo.issue(repo, f.issue)
-    rescue Octokit::NotFound
-      $loog.info("#{Fbe.issue(f)} doesn't exist in #{repo}")
-      f.stale = "issue ##{f.issue}"
-      $loog.info("#{Fbe.issue(f)} is lost")
-      next
-    end
-    a = json.dig(:user, :id)
-    if a.nil?
-      f.stale = 'who'
-      $loog.info("Authorship is lost in #{Fbe.issue(f)}")
-    else
-      f.who = a
-      $loog.info("Authorship is restored in #{Fbe.issue(f)}: #{f.who}")
+{
+  'issue-was-opened' => :user,
+  'pull-was-opened' => :user,
+  'issue-was-closed' => :closed_by,
+  'pull-was-closed' => :closed_by,
+  'pull-was-merged' => :merged_by
+}.each do |w, a|
+  Fbe.conclude do
+    quota_aware
+    on "(and
+      (eq where 'github')
+      (exists issue)
+      (exists repository)
+      (eq what '#{w}')
+      (not (exists stale))
+      (not (exists who)))"
+    consider do |f|
+      repo = Fbe.octo.repo_name_by_id(f.repository)
+      begin
+        json = Fbe.octo.issue(repo, f.issue)
+      rescue Octokit::NotFound
+        $loog.info("#{Fbe.issue(f)} doesn't exist in #{repo}")
+        f.stale = "issue ##{f.issue}"
+        $loog.info("#{Fbe.issue(f)} is lost")
+        next
+      end
+      who = json.dig(a, :id)
+      if who.nil?
+        f.stale = 'who'
+        $loog.info("Authorship is lost in #{Fbe.issue(f)}")
+      else
+        f.who = who
+        $loog.info("Authorship is restored in #{Fbe.issue(f)}: #{f.who}")
+      end
     end
   end
 end
