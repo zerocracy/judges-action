8bbfdd36835ca74420e7583a33a2ec2ee7a43749
Artem Yegorov <yegorov0725@yandex.ru>
2025-09-03 23:32:14 +0300

    #964: implement `code-was-reviewed` judge

---
+++ judges/code-was-reviewed/code-was-reviewed.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Zerocracy
+# SPDX-License-Identifier: MIT
+
+# Judge that monitors pull-was-merged or pull-was-closed facts with exists repository
+# and issue properties and create missing code-was-reviewed fact.
+
+require 'fbe/octo'
+require 'fbe/conclude'
+require 'fbe/issue'
+require 'fbe/who'
+
+Fbe.conclude do
+  on "(and
+    (or
+      (eq what 'pull-was-merged')
+      (eq what 'pull-was-closed'))
+    (exists repository)
+    (exists issue)
+    (eq where 'github')
+    (empty
+      (and
+        (eq issue $issue)
+        (eq repository $repository)
+        (eq where 'github')
+        (eq what '#{$judge}'))))"
+  consider do |f|
+    repo = Fbe.octo.repo_name_by_id(f.repository)
+    begin
+      pr = Fbe.octo.pull_request(repo, f.issue)
+      Fbe.octo.pull_request_reviews(repo, f.issue).each do |review|
+        next if review.dig(:user, :id) == pr.dig(:user, :id)
+        n =
+          Fbe.if_absent do |nn|
+            nn.where = f.where
+            nn.repository = f.repository
+            nn.issue = f.issue
+            nn.what = $judge
+            nn.who = review.dig(:user, :id)
+            nn.when = review[:submitted_at]
+          end
+        next if n.nil?
+        n.hoc = pr[:additions] + pr[:deletions]
+        n.author = pr.dig(:user, :id)
+        n.comments = Fbe.octo.issue_comments(repo, f.issue).count
+        n.review_comments = Fbe.octo.pull_request_review_comments(repo, f.issue, review[:id]).count
+        n.seconds = (review[:submitted_at] - pr[:created_at]).to_i
+        n.details = "The pull request #{Fbe.issue(n)} with #{n.hoc} HoC " \
+                    "created by #{Fbe.who(n, :author)} was reviewed by #{Fbe.who(n)} " \
+                    "after #{n.seconds / 3600}h#{(n.seconds % 3600) / 60}m and #{n.review_comments} comments."
+      end
+    rescue Octokit::NotFound
+      $loog.info("The pull request ##{f.issue} doesn't exist in #{repo}")
+      throw :rollback
+    end
+  end
+end
