410f0262961d85f134ddeea54976e1a5e391abfd
v <ost.vld@yandex.ru>
2025-09-05 02:47:19 +0300

    Quote paths to handle spaces and special chars

--- calc_coverage/run_cov.sh
+++ calc_coverage/run_cov.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 export INPUT_VERBOSE="true"
 echo "${INPUT_VERBOSE}"
 DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-${DIR}/pure_bash_coverage/generate_coverage_report.sh ${DIR}/../entry.sh
+"${DIR}/pure_bash_coverage/generate_coverage_report.sh" "${DIR}/../entry.sh"
 
