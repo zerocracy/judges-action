7c897570ea9833ef191c86306dfdb6eac26fb3ea
Artem Yegorov <yegorov0725@yandex.ru>
2025-09-28 23:36:36 +0300

    #1078: assign `branch` to fact in `find-latest-issue` judge

--- judges/find-earliest-issue/find-earliest-issue.rb
+++ judges/find-earliest-issue/find-earliest-issue.rb
@@ -31,6 +31,14 @@ Fbe.iterate do
       next if f.nil?
       f.when = json[:created_at]
       f.who = json.dig(:user, :id)
+      if json[:pull_request]
+        ref = Fbe.octo.pull_request(repo, f.issue).dig(:head, :ref)
+        if ref
+          f.branch = ref
+        else
+          f.stale = 'branch'
+        end
+      end
       f.details = "The issue #{Fbe.issue(f)} is the earliest we found, opened by #{Fbe.who(f)}."
       $loog.info("The opening of #{Fbe.issue(f)} by #{Fbe.who(f)} was found")
     end
