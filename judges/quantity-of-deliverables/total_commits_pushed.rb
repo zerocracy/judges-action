dd2789b9e7c1108d830b07d59e5d1c9c795903b2
Ghost <ghxstofsolana@gmail.com>
2026-05-28 02:42:19 +0200

    Skip transient commit count failures

--- judges/quantity-of-deliverables/total_commits_pushed.rb
+++ judges/quantity-of-deliverables/total_commits_pushed.rb
@@ -3,18 +3,43 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Zerocracy
 # SPDX-License-Identifier: MIT
 
+require 'fbe/github_graph'
 require 'fbe/octo'
 require 'fbe/unmask_repos'
+require 'net/http'
+require 'octokit'
 
 def total_commits_pushed(fact)
   commits = 0
   hoc = 0
   Fbe.unmask_repos do |repo|
-    next if Fbe.octo.repository(repo)[:size].zero?
+    repository =
+      begin
+        Fbe.octo.repository(repo)
+      rescue Octokit::NotFound, Octokit::Deprecated => e
+        $loog.info("#{repo} can't be inspected: #{e.class}: #{e.message}")
+        next
+      rescue Octokit::Forbidden => e
+        $loog.warn(
+          "[#{$judge}] Access forbidden to #{repo} " \
+          "(transient, will retry next cycle): #{e.class}: #{e.message}"
+        )
+        next
+      end
+    next if repository[:size].zero?
     owner, name = repo.split('/')
-    Fbe.github_graph.total_commits_pushed(owner, name, fact.since).then do |json|
-      commits += json['commits']
-      hoc += json['hoc']
+    begin
+      Fbe.github_graph.total_commits_pushed(owner, name, fact.since).then do |json|
+        commits += json['commits']
+        hoc += json['hoc']
+      end
+    rescue GraphQL::Client::Error, Octokit::Forbidden, Net::OpenTimeout, Net::ReadTimeout,
+           SocketError, Errno::ECONNRESET, Errno::ETIMEDOUT => e
+      $loog.warn(
+        "[#{$judge}] Can't count pushed commits in #{repo} " \
+        "(transient, will retry next cycle): #{e.class}: #{e.message}"
+      )
+      next
     end
   end
   {
