02b85190dfcb4e8ee56f9d3dab5f9d7b9537f0d2
VasilevNStas <ctac@outlook.com>
2026-05-31 22:01:04 +0300

    #760: Restore allow_http_connections, exclude _*.rb from SimpleCov

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -18,6 +18,7 @@ unless SimpleCov.running || ARGV.include?('--no-cov')
     add_filter 'vendor/'
     add_filter 'target/'
     add_filter 'test/smart_factbase.rb'
+    add_filter '_*.rb'
     track_files 'judges/**/*.rb'
     track_files 'lib/**/*.rb'
     track_files '*.rb'
@@ -42,6 +43,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'test/vcr_cassettes'
   config.hook_into(:webmock)
   config.ignore_request { |request| request.uri.include?('/rate_limit') }
+  config.allow_http_connections_when_no_cassette = true
   config.default_cassette_options = { record: :none, match_requests_on: %i[method uri], allow_playback_repeats: true }
 end
 
