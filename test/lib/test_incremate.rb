2b17502eae9c093ceef6e912bce486350eadb6c4
VasilevNStas <ctac@outlook.com>
2026-06-02 09:29:09 +0300

    Add max_per_fact unit test, lower fixture to exercise break path

--- test/lib/test_incremate.rb
+++ test/lib/test_incremate.rb
@@ -37,6 +37,46 @@ class TestIncremate < Minitest::Test
     $options = nil
   end
 
+  def test_incremate_respects_max_per_fact
+    WebMock.disable_net_connect!
+    stub_request(:get, 'https://api.github.com/rate_limit').to_return(
+      body: { rate: { remaining: 1000, limit: 1000 } }.to_json,
+      headers: { 'X-RateLimit-Remaining' => '999' }
+    )
+    $global = {}
+    $local = {}
+    $loog = Loog::VERBOSE
+    $options = Judges::Options.new({ 'lifetime' => 100, 'timeout' => 100 })
+    Dir.mktmpdir do |dir|
+      File.write(File.expand_path('some_alpha.rb', dir), <<~RUBY)
+        def some_alpha(_f)
+          { some_alpha: 1 }
+        end
+      RUBY
+      File.write(File.expand_path('some_beta.rb', dir), <<~RUBY)
+        def some_beta(_f)
+          { some_beta: 2 }
+        end
+      RUBY
+      File.write(File.expand_path('some_gamma.rb', dir), <<~RUBY)
+        def some_gamma(_f)
+          { some_gamma: 3 }
+        end
+      RUBY
+      time = Time.now - 60
+      fb = Factbase.new
+      f = fb.insert
+      Jp.incremate(f, dir, 'some', max_per_fact: 2, epoch: time, kickoff: time)
+      assert(f.some_alpha, 'alpha should be set')
+      assert(f.some_beta, 'beta should be set')
+      assert_nil(f['some_gamma'], 'gamma should NOT be set (max_per_fact=2 caps at 2)')
+    end
+    $global = nil
+    $local = nil
+    $loog = nil
+    $options = nil
+  end
+
   def test_incremate_pauses_between_evaluations
     WebMock.disable_net_connect!
     stub_request(:get, 'https://api.github.com/rate_limit').to_return(
