1ea1becd998c76aad535577a7d32b0a06f68b0d1
VasilevNStas <ctac@outlook.com>
2026-06-02 16:18:14 +0300

    #1272: reduce cron to */30, first 403 logs error (not crash), subsequent warn

--- judges/github-events/github-events.rb
+++ judges/github-events/github-events.rb
@@ -291,10 +291,22 @@ Fbe.iterate do
       skip(json)
     end
   rescue Octokit::Forbidden => e
-    $loog.warn(
-      "[#{$judge}] Access forbidden to #{rname} " \
-      "(transient, will retry next cycle): #{e.class}: #{e.message}"
-    )
+    if $global[:forbidden_repos]&.include?(rname)
+      $loog.warn(
+        "[#{$judge}] Access forbidden to #{rname} " \
+        "(transient, will retry next cycle): #{e.class}: #{e.message}"
+      )
+    else
+      $global[:forbidden_repos] ||= Set.new
+      $global[:forbidden_repos] << rname
+      who =
+        begin
+          "@#{Fbe.octo.user[:login]}"
+        rescue Octokit::Forbidden
+          'You'
+        end
+      $loog.error("[#{$judge}] #{who} doesn't have access to the #{rname} repository, maybe it is private")
+    end
     skip(json)
   end
 
