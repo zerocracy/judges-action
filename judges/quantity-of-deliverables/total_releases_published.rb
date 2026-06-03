d8a3dc1c5d2a098d6e503b7a19c16bfe47f615e3
Ghost <ghxstofsolana@gmail.com>
2026-06-03 20:11:29 +0200

    Handle transient release-count errors in total_releases_published

--- judges/quantity-of-deliverables/total_releases_published.rb
+++ judges/quantity-of-deliverables/total_releases_published.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
+require 'fbe/github_graph'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Zerocracy
 # SPDX-License-Identifier: MIT
 
 require 'fbe/octo'
 require 'fbe/unmask_repos'
+require 'net/http'
+require 'octokit'
 
 def total_releases_published(fact)
-  {
-    total_releases_published:
-      Fbe.unmask_repos.sum do |repo|
-        owner, name = repo.split('/')
+  total =
+    Fbe.unmask_repos.sum do |repo|
+      owner, name = repo.split('/')
+      begin
         Fbe.github_graph.total_releases_published(owner, name, fact.since)['releases']
+      rescue GraphQL::Client::Error, Octokit::Forbidden, Net::OpenTimeout, Net::ReadTimeout,
+             SocketError, Errno::ECONNRESET, Errno::ETIMEDOUT => e
+        $loog.warn(
+          "[#{$judge}] Can't count released projects in #{repo} " \
+          "(transient, will retry next cycle): #{e.class}: #{e.message}"
+        )
+        next 0
       end
-  }
+    end
+  { total_releases_published: total }
 end
