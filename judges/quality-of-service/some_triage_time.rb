1085dea3f40aa131a8b57d453cce7b3528949325
VasilevNStas <ctac@outlook.com>
2026-06-09 14:35:07 +0300

    #1532: rescue Octokit errors in some_triage_time repo_id_by_name call

--- judges/quality-of-service/some_triage_time.rb
+++ judges/quality-of-service/some_triage_time.rb
@@ -17,10 +17,23 @@ def some_triage_time(fact)
     found = Jp.qosearch("repo:#{repo} type:issue created:#{fact.since.utc.iso8601}..#{fact.when.utc.iso8601}")
     return {} if found.nil?
     found[:items].each do |issue|
+      rid = # rubocop:disable Elegant/NoRedundantVariable
+        begin
+          Fbe.octo.repo_id_by_name(repo)
+        rescue Octokit::NotFound, Octokit::Deprecated => e
+          $loog.info("Repository #{repo} not found: #{e.message}")
+          next
+        rescue Octokit::Forbidden => e
+          $loog.warn(
+            "[#{$judge}] Access forbidden to #{repo} " \
+            "(transient, will retry next cycle): #{e.class}: #{e.message}"
+          )
+          next
+        end
       ff = Fbe.fb.query(
         "
         (and
-          (eq repository #{Fbe.octo.repo_id_by_name(repo)})
+          (eq repository #{rid})
           (eq issue #{issue[:number]})
           (eq what 'label-was-attached')
           (or
