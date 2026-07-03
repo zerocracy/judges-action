4f28a2e506967396241df21c675a3652d2297c20
VasilevNStas <ctac@outlook.com>
2026-07-04 00:17:05 +0300

    #1664: add reactions cache to reduce N+1 API calls in count_appreciated_comments

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -64,9 +64,11 @@ end
 
 def Jp.count_appreciated_comments(pr, issue_comments, code_comments, repo: nil)
   repo = pr.dig(:base, :repo, :full_name) if repo.nil?
+  @_reactions ||= {}
   issue_comments.sum do |comment|
-    Fbe.octo.issue_comment_reactions(repo, comment[:id])
-      .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
+    @_reactions[comment[:id]] ||=
+      Fbe.octo.issue_comment_reactions(repo, comment[:id])
+        .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
   rescue Octokit::NotFound, Octokit::Deprecated => e
     $loog.info("Issue comment ##{comment[:id]} reactions don't exist in #{repo}: #{e.message}")
     0
@@ -77,8 +79,9 @@ def Jp.count_appreciated_comments(pr, issue_comments, code_comments, repo: nil)
     )
     0
   end + code_comments.sum do |comment|
-    Fbe.octo.pull_request_review_comment_reactions(repo, comment[:id])
-      .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
+    @_reactions[comment[:id]] ||=
+      Fbe.octo.pull_request_review_comment_reactions(repo, comment[:id])
+        .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
   rescue Octokit::NotFound, Octokit::Deprecated => e
     $loog.info("Code comment ##{comment[:id]} reactions don't exist in #{repo}: #{e.message}")
     0
