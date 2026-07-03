61f30fea9df1fb6e250b921a6eb6b014658029b6
Vasilev Stas <33578155+VasilevNStas@users.noreply.github.com>
2026-07-03 13:00:46 +0300

    #1580: validate property names in fill_fact_by_hash (#1629)

--- lib/fill_fact.rb
+++ lib/fill_fact.rb
@@ -7,6 +7,7 @@ require_relative 'jp'
 
 def Jp.fill_fact_by_hash(fact, hash)
   hash.each do |prop, value|
+    raise(ArgumentError, "Invalid property name: #{prop.inspect}") unless /^[a-z][a-z_0-9]*$/i.match?(prop.to_s)
     fact.__send__(:"#{prop}=", value)
   end
 end
