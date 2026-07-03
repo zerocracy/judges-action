61ba6b6e51482c1060d4d50041739f9e5cb098d9
VasilevNStas <ctac@outlook.com>
2026-07-04 00:23:58 +0300

    #1720: wrap delete! and stale marking in a transaction for atomicity

--- judges/who-is-alive/who-is-alive.rb
+++ judges/who-is-alive/who-is-alive.rb
@@ -40,12 +40,14 @@ Fbe.consider(
     next
   end
   elapsed($loog, level: Logger::INFO) do
-    Fbe.fb.query("(and (eq where 'github') (eq what 'who-has-name') (eq who #{f.who}))").delete!
-    done =
-      Fbe.fb.query("(and (eq where 'github') (eq who #{f.who}))").each do |n|
-        n.stale = 'who'
-      end
-    throw :"GitHub user ##{f.who} is gone, marked #{done} facts as stale"
+    Fbe.fb.txn do |fbt|
+      fbt.query("(and (eq where 'github') (eq what 'who-has-name') (eq who #{f.who})").delete!
+      done =
+        fbt.query("(and (eq where 'github') (eq who #{f.who}))").each do |n|
+          n.stale = 'who'
+        end
+      throw :"GitHub user ##{f.who} is gone, marked #{done} facts as stale"
+    end
   end
 end
 
