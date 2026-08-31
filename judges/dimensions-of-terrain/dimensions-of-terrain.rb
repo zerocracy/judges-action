fef89ca8c00ebbbb71482b90a6d1391de48acbe0
morphqdd <europeDreadlyDevil@gmail.com>
2026-08-31 17:51:41 +0300

    #1906: date the fact by the clock the query filters on (#1927)

--- judges/dimensions-of-terrain/dimensions-of-terrain.rb
+++ judges/dimensions-of-terrain/dimensions-of-terrain.rb
@@ -8,15 +8,24 @@ require 'fbe/octo'
 require 'time'
 require_relative '../../lib/incremate'
 
+today =
+  begin
+    v = ENV.fetch('TODAY', nil)
+    if v.nil? || v.empty?
+      Time.now.utc
+    else
+      Time.parse(v)
+    end
+  end
 f = Fbe.fb.query(
   "(and
     (eq what '#{$judge}')
-    (gt when (minus (to_time (env 'TODAY' '#{Time.now.utc.iso8601}')) '1 days')))"
+    (gt when (minus (to_time '#{today.utc.iso8601}') '1 days')))"
 ).each.first
 if f.nil?
   f = Fbe.fb.insert
   f.what = $judge
-  f.when = Time.now
+  f.when = today
 end
 
 Jp.incremate(f, __dir__, 'total')
