3470fbc7444162e8dbb619bdfe142fb7484bb620
Thayorns <thayornswordsman@gmail.com>
2026-09-01 19:47:30 +0300

    fix(#1944): repair failing tests to match rubocop and tombstone-bury behavior

--- test/judges/test-code-was-reviewed.rb
+++ test/judges/test-code-was-reviewed.rb
@@ -282,12 +282,8 @@ class TestCodeWasReviewed < Jp::Test
         { id: 49_112, user: { id: 422, login: 'user2' }, submitted_at: Time.parse('2025-09-02 10:39:20 UTC') }
       ]
     )
-    stub_github(
-      'https://api.github.com/repos/foo/foo/issues/44/comments?per_page=100', body: []
-    )
-    stub_github(
-      'https://api.github.com/repos/foo/foo/pulls/44/reviews/49112/comments?per_page=100', body: []
-    )
+    stub_github('https://api.github.com/repos/foo/foo/issues/44/comments?per_page=100', body: [])
+    stub_github('https://api.github.com/repos/foo/foo/pulls/44/reviews/49112/comments?per_page=100', body: [])
     stub_github('https://api.github.com/user/421', body: { id: 421, login: 'user1' })
     stub_github('https://api.github.com/user/422', body: { id: 422, login: 'user2' })
     fb = Factbase.new
@@ -297,8 +293,8 @@ class TestCodeWasReviewed < Jp::Test
       fb.one?(what: 'code-was-reviewed', repository: 42, issue: 44, who: 422),
       'a review left by a live account must be credited even when a deleted account reviewed too'
     )
-    refute(
-      fb.one?(what: 'code-was-reviewed', repository: 42, issue: 44, who: nil),
+    assert_equal(
+      1, fb.query("(and (eq what 'code-was-reviewed') (eq repository 42) (eq issue 44))").each.to_a.size,
       'a review left by a deleted account cannot produce a fact'
     )
   end
