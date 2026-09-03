020c2e86f75c3a4c8094149d55991598868a3551
Thayorns <thayornswordsman@gmail.com>
2026-09-03 09:54:37 +0300

    fix(#1990): guard total_contributors against a nil contributor id

--- judges/dimensions-of-terrain/total_contributors.rb
+++ judges/dimensions-of-terrain/total_contributors.rb
@@ -39,7 +39,8 @@ def total_contributors(_fact)
       end
     next unless list.is_a?(Array)
     list.each do |contributor|
-      contributors << contributor[:id]
+      id = contributor[:id]
+      contributors << id unless id.nil?
     end
   rescue Octokit::NotFound, Octokit::Deprecated => e
     $loog.info("Repository/contributors info not found for #{repo}: #{e.message}")
