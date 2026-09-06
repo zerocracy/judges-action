6e64c24a801d66172de7d5c2e13b85ddb62cb3a7
devil <europedreadlydevil@gmail.com>
2026-09-06 16:17:59 +0000

    #1664: do not ask about reactions of a comment that has none

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -95,27 +95,21 @@ end
 
 def Jp.count_appreciated_comments(pr, issue_comments, code_comments, repo: nil)
   repo = pr.dig(:base, :repo, :full_name) if repo.nil?
-  issue_comments.sum do |comment|
-    Fbe.octo.issue_comment_reactions(repo, comment[:id])
+  Jp.appreciations(repo, issue_comments, 'Issue', :issue_comment_reactions) +
+    Jp.appreciations(repo, code_comments, 'Code', :pull_request_review_comment_reactions)
+end
+
+def Jp.appreciations(repo, comments, kind, method)
+  comments.sum do |comment|
+    next 0 if comment.dig(:reactions, :total_count)&.zero?
+    Fbe.octo.public_send(method, repo, comment[:id])
       .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
   rescue Octokit::NotFound, Octokit::Deprecated => e
-    $loog.info("Issue comment ##{comment[:id]} reactions don't exist in #{repo}: #{e.message}")
+    $loog.info("#{kind} comment ##{comment[:id]} reactions don't exist in #{repo}: #{e.message}")
     0
   rescue Octokit::Forbidden => e
     $loog.warn(
-      "Access forbidden to issue comment ##{comment[:id]} reactions in #{repo} " \
-      "(transient, will retry next cycle): #{e.class}: #{e.message}"
-    )
-    0
-  end + code_comments.sum do |comment|
-    Fbe.octo.pull_request_review_comment_reactions(repo, comment[:id])
-      .count { |reaction| reaction.dig(:user, :id) != comment.dig(:user, :id) }
-  rescue Octokit::NotFound, Octokit::Deprecated => e
-    $loog.info("Code comment ##{comment[:id]} reactions don't exist in #{repo}: #{e.message}")
-    0
-  rescue Octokit::Forbidden => e
-    $loog.warn(
-      "Access forbidden to code comment ##{comment[:id]} reactions in #{repo} " \
+      "Access forbidden to #{kind.downcase} comment ##{comment[:id]} reactions in #{repo} " \
       "(transient, will retry next cycle): #{e.class}: #{e.message}"
     )
     0
