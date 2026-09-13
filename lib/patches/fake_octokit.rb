f2e650cdc16ae71e95eba50be13ba49d748fef76
canburakyol <canburakyol@users.noreply.github.com>
2026-09-13 11:27:07 +0300

    #2173: pin forks count in fake and measure-all expectation

--- lib/patches/fake_octokit.rb
+++ lib/patches/fake_octokit.rb
@@ -6,6 +6,17 @@
 require 'fbe/octo'
 
 class Fbe::FakeOctokit
+  unless method_defined?(:orig_repository)
+    alias_method :orig_repository, :repository
+  end
+
+  def repository(name)
+    h = orig_repository(name)
+    h[:forks] = 1
+    h[:forks_count] = 1
+    h
+  end
+
   def list_milestones(_repo, _options = {})
     []
   end
