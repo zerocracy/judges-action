29dbb7d60e75e4d13ee588fde3afe7e12f3401f3
devil <europedreadlydevil@gmail.com>
2026-09-19 07:29:19 +0000

    #2364: test for an issue whose author is gone

--- test/judges/test-issue-was-opened.rb
+++ test/judges/test-issue-was-opened.rb
@@ -27,6 +27,24 @@ class TestIssueWasOpened < Jp::Test
     )
   end
 
+  def test_survives_an_issue_whose_author_is_gone
+    fb = Factbase.new
+    fb.with(_id: 1, what: 'issue-was-closed', repository: 42, issue: 44, where: 'github')
+    Jp::FakeGithub.new(
+      'GET /rate_limit' => { resources: { search: { remaining: 30, limit: 30 } }, rate: { remaining: 1000 } },
+      'GET /repositories/42' => { id: 42, full_name: 'foo/foo' },
+      'GET /repos/foo/foo' => { id: 42, full_name: 'foo/foo' },
+      'GET /repos/foo/foo/issues/44' => {
+        number: 44, created_at: Time.parse('2025-01-01 12:00:00 UTC'), user: nil
+      }
+    ).run do
+      load_it('issue-was-opened', fb)
+    end
+    f = fb.picks(what: 'issue-was-opened').first
+    refute_nil(f, 'no fact was made for an issue whose author deleted the account')
+    assert_equal('who', f['stale'].first)
+  end
+
   def test_rescues_not_found_on_repo_name_lookup
     fb = Factbase.new
     fb.with(_id: 1, what: 'issue-was-closed', repository: 42, issue: 44, where: 'github')
